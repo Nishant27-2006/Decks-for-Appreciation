28e1774094419ec4fa07a0e8f957a958070d776c
Nishant Gadde <nishantg2706@gmail.com>
2024-01-14 14:45:50 -0600

    Initial Commit

---
+++ Decks for Appreciation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Decks for Appreciation
+//
+//  Created by Neelima Gadde on 1/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Decks for Appreciation/Decks_for_AppreciationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Decks_for_AppreciationApp.swift
+//  Decks for Appreciation
+//
+//  Created by Neelima Gadde on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Decks_for_AppreciationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
