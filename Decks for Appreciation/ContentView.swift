835f7bcff441cc4912e49b51b63692a39cc20dfd
Nishant Gadde <nishantg2706@gmail.com>
2024-01-14 15:45:57 -0600

    added login title

--- Decks for Appreciation/ContentView.swift
+++ Decks for Appreciation/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Decks for Appreciation")
+            Text("Login: ")
         }
         .padding()
     }
