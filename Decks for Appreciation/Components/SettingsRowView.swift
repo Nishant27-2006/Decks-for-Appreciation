c7504a6a0a78449b4866281565a114a3e576272a
Nishant Gadde <nishantg2706@gmail.com>
2024-01-14 18:13:23 -0600

    commited all these changes please push

--- Decks for Appreciation/Components/SettingsRowView.swift
+++ Decks for Appreciation/Components/SettingsRowView.swift
@@ -5,4 +5,27 @@
 //  Created by Neelima Gadde on 1/14/24.
 //
 
-import Foundation
+import SwiftUI
+struct SettingsRowView: View{
+    let imageName: String
+    let title: String
+    let tintColor: Color
+    
+    var body: some View{
+        HStack(spacing: 12){
+            Image(systemName: imageName)
+                .imageScale(.small)
+                .font(.title)
+                .foregroundColor(tintColor)
+            
+            Text(title)
+                .font(.subheadline).foregroundColor(.black)
+        }
+    }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+    }
+}
