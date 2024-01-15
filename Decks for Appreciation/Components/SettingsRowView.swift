9248df9bf8c7479771917fc58f1156a6d09998ce
Nishant Gadde <nishantg2706@gmail.com>
2024-01-14 18:08:31 -0600

    almost done with login changes

---
+++ Decks for Appreciation/Components/SettingsRowView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsRowView.swift
+//  Decks for Appreciation
+//
+//  Created by Neelima Gadde on 1/14/24.
+//
+
+import Foundation
---
+++ Decks for Appreciation/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Decks for Appreciation
+//
+//  Created by Neelima Gadde on 1/14/24.
+//
+
+import Foundation
